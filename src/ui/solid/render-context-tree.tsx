f220d29fb9513560291f55f3cbe700270894a988
Ivan Liadniou <ivan_liadniou@epam.com>
2023-07-15 12:15:39 +0200

    Add configs for SolidJS and Jest

--- src/ui/solid/render-context-tree.tsx
+++ src/ui/solid/render-context-tree.tsx
@@ -1,28 +1,31 @@
-import { FileContextTree } from "../components/context-tree/types";
-import SNWPlugin from "../../main";
 import { render } from "solid-js/web";
 import { PluginContextProvider } from "./plugin-context";
 import { Tree } from "./tree";
+import { FileContextTree } from "../../context-tree/types";
+import BetterBacklinksPlugin from "../../plugin";
 
 interface RenderContextTreeProps {
-  contextTrees: FileContextTree[];
-  resultCount: number;
-  el: HTMLElement;
-  plugin: SNWPlugin;
+	contextTrees: FileContextTree[];
+	resultCount: number;
+	el: HTMLElement;
+	plugin: BetterBacklinksPlugin;
 }
 
 export function renderContextTree({
-  contextTrees,
-  resultCount,
-  el,
-  plugin,
+	contextTrees,
+	resultCount,
+	el,
+	plugin,
 }: RenderContextTreeProps) {
-  render(
-    () => (
-      <PluginContextProvider plugin={plugin}>
-        <Tree fileContextTrees={contextTrees} resultCount={resultCount} />
-      </PluginContextProvider>
-    ),
-    el
-  );
+	render(
+		() => (
+			<PluginContextProvider plugin={plugin}>
+				<Tree
+					fileContextTrees={contextTrees}
+					resultCount={resultCount}
+				/>
+			</PluginContextProvider>
+		),
+		el
+	);
 }
