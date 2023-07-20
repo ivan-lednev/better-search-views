40939c916c21a2f130f16ae1796dbc31b78d694d
Ivan Liadniou <ivan_liadniou@epam.com>
2023-07-20 14:27:11 +0200

    Fix checkbox alignment

--- src/ui/solid/match-section.tsx
+++ src/ui/solid/match-section.tsx
@@ -21,7 +21,7 @@ export function MatchSection(props: MatchSectionProps) {
 
             return (
               <div
-                class="search-result-file-match better-search-views-file-match"
+                class="search-result-file-match better-search-views-file-match markdown-preview-view"
                 ref={async (el) => {
                   await MarkdownRenderer.renderMarkdown(
                     section.text,
